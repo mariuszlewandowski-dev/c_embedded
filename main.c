dd8a8c8e1ff063678fe9c7a754098fb8d7ea3baa
lewandowskimariusz <mariusz.k.lewandowskI@gmail.com>
2024-08-23 11:42:58 +0200

    new file:   main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int my_favorite = 0;
+    printf("Please enter yp\n");
+
+    scanf("%d", &my_favorite);
+    printf("You entered number: %d\n0", my_favorite);
+    return 0;
+
+}
